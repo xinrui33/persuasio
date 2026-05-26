00c5fd25077cd18ae8a7f783fc5c4171eee62258
xinrui33 <xc2731@columbia.edu>
2026-05-26 11:35:50 -0400

    feat: GKB dataset

---
+++ R/GKB.R
@@ -0,0 +1,14 @@
+#' GKB Dataset
+#'
+#' Data from Gerber, Karlan, and Bergan (2009) on media persuasion effects.
+#' Used as the example dataset for the persuasio package.
+#'
+#' @format A data frame with 701 rows and 4 binary (0/1) variables:
+#' \describe{
+#'   \item{post}{Post-treatment indicator}
+#'   \item{voteddem_all}{Voted Democratic (outcome y)}
+#'   \item{readsome}{Read newspaper treatment (treatment t)}
+#'   \item{MZwave2}{Instrument z}
+#' }
+#' @source Gerber, Karlan, and Bergan (2009)
+"GKB"
